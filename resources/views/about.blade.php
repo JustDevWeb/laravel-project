30d4641627110657caf2cb582cefd95615aa224f
Mikhail1JS <misha.zaichko@gmail.com>
2023-03-13 18:04:41 +0500

    lesson1:task: add 3 routes

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+</head>
+<body>
+<h1>About project</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, non, rerum. Cum cumque deleniti eius esse et explicabo, harum incidunt ipsa magnam molestiae nesciunt possimus, quis repudiandae rerum sint ut voluptatem voluptates. Accusamus accusantium, aperiam autem, corporis dicta dolorem doloribus dolorum esse eum fugit harum id illo impedit iure iusto laboriosam magni maxime molestiae nam natus odit optio perspiciatis praesentium provident quaerat quia repellendus rerum sapiente sunt tempore temporibus totam velit voluptatibus. Asperiores, consequuntur cum dolore dolores eaque eligendi maxime minus, odit possimus, vitae voluptates voluptatibus. Accusantium aperiam autem distinctio error hic, labore odit provident repudiandae sapiente sequi temporibus vitae?</p>
+</body>
+</html>
